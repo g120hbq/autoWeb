f96c5f275fd73c96eb3ae41c7afe073b84433700
hubiqing <hbqhbq123@qq.com>
2015-12-28 19:44:25 +0800

    writefile

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /**
  * Created by biqing.hu on 2015/12/16.
  */
+var writefile = require('writefile');
 var server = module.exports =require('./server');
 server.listen(process.env.PORT || 4123, function () {
     console.log('app listened');
@@ -28,11 +29,13 @@ var openBrowser = function (url){
 }
 
 var writePort = function (port){
-    var cmd='echo '+port+' > port.txt ';
-    child_process.exec(cmd,
-        function (error, stdout, stderr) {
-            if (error !== null) {
-                console.log('exec error: cmd->' + cmd);
-            }
-        });
+    //var cmd='echo '+port+' > port.txt ';
+    //child_process.exec(cmd,
+    //    function (error, stdout, stderr) {
+    //        if (error !== null) {
+    //            console.log('exec error: cmd->' + cmd);
+    //        }
+    //    });
+    writefile('port.txt',port+'');
+    console.log('writefile:'+'port.txt'+'['+port+']');
 }
