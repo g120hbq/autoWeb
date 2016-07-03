ec2e5b80ff6d3e55298509136ed1c87c6d2b3465
hubiqing <hbqhbq123@qq.com>
2016-07-04 00:38:38 +0800

    'base64处理'

--- server/routers/api.js
+++ server/routers/api.js
@@ -40,3 +40,20 @@ router.delete('/users/:id',function (req, res, next) {
     });
 });
 
+
+
+//base64保存为图片
+router.post('/upload', function(req, res){
+//接收前台POST过来的base64
+    var imgData = req.body.imgData;
+//过滤data:URL
+    var base64Data = imgData.replace(/^data:image\/\w+;base64,/,"");
+    var dataBuffer = new Buffer(base64Data, 'base64');
+    fs.writeFile("image.png", dataBuffer, function(err) {
+        if(err){
+            res.send(err);
+        }else{
+            res.send("保存成功！");
+        }
+    });
+});
